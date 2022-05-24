afce63538d789b1762460af698da3c1eb312bd62
iljorlov <iljorlov@gmail.com>
2022-05-24 16:37:54 +0300

    deployment changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
+  buildDir: "dist",
   experiments: {
     asyncWebAssembly: true,
     syncWebAssembly: true
